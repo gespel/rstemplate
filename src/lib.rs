ae8d26505b74232675a083b30b2c8e92194aee17
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2023-11-06 18:07:15 +0100

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+pub struct Tester {
+    #[pyo3(get, set)]
+    pub name: String,
+    #[pyo3(get, set)]
+    pub age: i64
+}
+#[pymethods]
+impl Tester {
+    #[new]
+    pub fn new(data: String, age: i64) -> Tester {
+        Tester {
+            name: data,
+            age: age,
+        }
+    }
+    pub fn printer(&self) {
+        println!("{}", &self.name);
+        println!("{}", &self.age);
+    }
+}
+
+
 #[pyfunction]
 fn test(a: i64) -> PyResult<i64> {
     Ok(a + 1)
@@ -8,5 +31,6 @@ fn test(a: i64) -> PyResult<i64> {
 #[pymodule]
 fn rstemplate(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(test, m)?)?;
+    m.add_class::<Tester>()?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import rstemplate
 
 x = rstemplate.test(2)
+c = rstemplate.Tester("sten", 25)
 print(x)
+print(c.printer())
